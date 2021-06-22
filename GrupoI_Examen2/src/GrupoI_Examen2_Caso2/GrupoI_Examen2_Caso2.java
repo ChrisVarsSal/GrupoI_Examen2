77039a23b521478387d57ee75eb4c6d2d4c8a3db
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2021-06-22 13:39:51 -0600

    A4

--- GrupoI_Examen2/src/GrupoI_Examen2_Caso2/GrupoI_Examen2_Caso2.java
+++ GrupoI_Examen2/src/GrupoI_Examen2_Caso2/GrupoI_Examen2_Caso2.java
@@ -1,27 +1,5 @@
 package GrupoI_Examen2_Caso2;
-/*
-Patron escogido: Observer
-motivo:
-Por que no usar otros patrones????
-1-al momento de procesar la cantidad de pantallas se podria pensar en utilizar el patron strategy
-sin embargo usarlo implicaria saber *apriori* cuantas pantallas tengo, para escoger INTENCIONALMENTE
-una estrategia y este punto de escoger intencionalmete la estrategia es lo que vuelve aparte de compicado
-el codigo, poco extensible con lo que matamos su manteniblilidad...
-pensemos en lo que tendriamos que hacer al aplicar el patron Strategy...
-tendriamos que preguntar PARA CADA ESTRATEGIA cual es la cantidad de pantallas, con lo cual para solo un
-cliente necesitariamos n cantidad de sentencias if-else para ejectutar el algoritmo apropiado, siendo n
-la cantidad de posibles estrategias para calcular el costo final..
-con lo que si deseamos agregar una nueva estrategia debemos agregar una nueva sentencia if else a nuestro codigo
-con lo cual estariamos rompiendo el principio abierto cerrado...
-y esta logica queda cerrada a valores particulares de numero de pantallas..
-por ende si el cliente cambia de numero de pantallas necesitamos una logica especializada para esto
-donde los cambios con los algoritmos anteriores serian casi minimos llenando nuestro codigo de lineas innecesarias 
-si usamos el patron observador, que publica un cliente y cada observador se encarga de darle un monto final 
-a pagar al usuario...
-y si en el futuro desearamos incluir un descuento, impuesto etc...
-podemos simplemente agregar un observador y subscribirlo
-dejando nuestro codigo abierto a la  extension promoviendo una mejor mantenibilidad.
-*/
+
 public class GrupoI_Examen2_Caso2{
 
     /**
